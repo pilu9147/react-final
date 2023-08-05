646a4853d7dfcd1159f72c76a7ec65a401c0d5ba
pilu9147 <priyankabhesaniya617@gmail.com>
2023-08-06 01:02:47 +0530

    chn

--- src/components/Clicked.jsx
+++ src/components/Clicked.jsx
@@ -1,5 +1,5 @@
 // Clicked.js
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState,useRef } from 'react';
 import { useSelector } from 'react-redux';
 import { AiOutlineShareAlt } from 'react-icons/ai';
 import { HiOutlineHeart } from 'react-icons/hi';
@@ -12,10 +12,14 @@ const Clicked = () => {
   const { postdata, curr } = useSelector((state) => state);
   const [display, setDisplay] = useState(null);
   const [activeButton, setActiveButton] = useState('details');
-
+  const detailSectionRef = useRef(null);
   useEffect(() => {
     const post = postdata.find((d) => d.id === curr);
     setDisplay(post || null);
+
+    if (detailSectionRef.current) {
+      detailSectionRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
   }, [curr, postdata]);
 
   return (
@@ -26,7 +30,7 @@ const Clicked = () => {
         <NavLink to='/'> <BiLeftArrowCircle style={{color:'black',backgroundColor:'#fff',fontSize:'40px',marginRight:'7px',border:'none'}}/></NavLink>
           <h1>Post Number #{display.id}</h1>
          </div>
-          <div className='postdetail'>
+          <div className='postdetail' ref={detailSectionRef}>
             <div className='d'>
               <div
                 className='imgdiv'
@@ -76,77 +80,6 @@ const Clicked = () => {
 
 export default Clicked;
 
-// import React, { useEffect, useState } from 'react';
-// import { useSelector } from 'react-redux';
-// import { AiOutlineShareAlt } from 'react-icons/ai';
-// import { HiOutlineHeart } from 'react-icons/hi';
-// import './clicke.css';
-// import Posts from './Posts';
-
-// const Clicked = () => {
-//   const { postdata, curr } = useSelector((state) => state);
-//   console.log(postdata, curr);
-
-//   const [display, setDisplay] = useState(null);
-//   const [activeButton, setActiveButton] = useState('details'); // Set the initial active button
- 
-
-//   useEffect(() => {
-//     const post = postdata.find((d) => d.id === curr);
-//     setDisplay(post || null);
-//   }, [curr, postdata]);
-
-//   console.log(display);
-
-//   return (
-//     <div style={{ margin: '20px 50px' }}>
-//       {display && (
-//         <>
-//           <h1>Post Number #{display.id}</h1>
-//           <div className='postdetail'>
-//             <div className='d'>
-//               <div
-//                 className='imgdiv'
-//                 style={{
-//                   backgroundImage: `url(https://picsum.photos/200?random=${display.id})`,
-//                 }}
-//               >
-//                 <div className='con'>
-//                   <p className='ttl'>{display.title}</p>
-//                   <div className='right'>
-//                     <AiOutlineShareAlt />
-//                     <HiOutlineHeart />
-//                   </div>
-//                 </div>
-//               </div>
-//             </div>
-//             <div className='uinfo'>
-//               <button
-//                 className={activeButton === 'details' ? 'active' : ''}
-//                 onClick={() => setActiveButton('details')}
-//               >
-//                 Details
-//               </button>
-//               <button
-//                 className={activeButton === 'userInfo' ? 'active' : ''}
-//                 onClick={() => setActiveButton('userInfo')}
-//               >
-//                 User Info
-//               </button>
-//               <p> {activeButton === 'details' ? display.body : `Post Was Posted By ${display.userId}.`}</p>
-//             </div>
-//           </div>
-
-//           <h1>More Posts</h1>
-
-//           <Posts />
-//         </>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default Clicked;
 
 
 
